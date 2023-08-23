045326b0702270de640b2f837495ebfb9fecedbe
Lxghtend <140092221+Lxghtend@users.noreply.github.com>
2023-08-23 12:37:28 -0400

    Update Auto Discord Message Sender.py
    Made it EVEN easier to use

--- Auto Discord Message Sender.py
+++ Auto Discord Message Sender.py
@@ -12,7 +12,7 @@ header = {
 }
 
 async def tc_sales_post_request():
-    r = requests.post("https://discord.com/api/v9/channels/845080693175222282/messages", data=payload, headers=header)
+    r = requests.post("https://discord.com/api/v9/channels/845080693175222282/messages", data=payload, headers=header) # change the discord api link to the channel u want link
     print("Message sent")
     await asyncio.sleep(1800)  #<---- change this number to change time between message (seconds)
 
